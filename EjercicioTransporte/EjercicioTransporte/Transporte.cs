36954429af1d4d334d045a354e4eb0afefacd47b
Franco Amato <francoamato92@gmail.com>
2021-04-16 22:36:01 -0300

    ejercicio 1

---
+++ EjercicioTransporte/EjercicioTransporte/Transporte.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioTransporte
+{
+    public abstract class Transporte
+    {
+        public int Pasajeros { get; }
+
+        public Transporte (int pasajeros)
+        {
+            this.Pasajeros = pasajeros;
+        }
+
+        public abstract string Avanzar();
+
+        public abstract string Detener();
+
+        public abstract string Descripcion();
+
+    }
+}
