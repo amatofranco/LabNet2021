1ece4609e19e0eca5b74def7599c83e35c0da1b7
Franco Amato <francoamato92@gmail.com>
2021-04-19 10:25:28 -0300

    Ejercicio 2

---
+++ EjercicioExceptions/EjercicioExceptions/Logic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioExceptions
+{
+    public class Logic
+    {
+        public static void ThrowException()
+        {
+            throw new InvalidOperationException("Se disparo una Excepcion");
+            
+        }
+
+        public static void ThrowCustomException()
+        {
+            throw new CustomException("Se disparo una Excepcion");
+        }
+    }
+}
