455b82c8555d73615bed1ee885e1c247e3a50867
Franco Amato <francoamato92@gmail.com>
2021-05-02 02:24:04 -0300

    Ejercicio ef

---
+++ EjercicioEF/EjercicioEF.Logic/BaseLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EjercicioEF.Data;
+
+namespace EjercicioEF.Logic
+{
+    public class BaseLogic
+    {
+        protected NorthwindContext context;
+
+        public BaseLogic()
+        {
+            context = new NorthwindContext();
+        }
+    }
+}
---
+++ EjercicioEF/EjercicioEF.Logic/CategoriesLogic.cs
@@ -0,0 +1,69 @@
+﻿using EjercicioEF.Entities;
+using EjercicioEF.Logic.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioEF.Logic
+{
+    public class CategoriesLogic : BaseLogic
+    {
+        public List<Categories> GetList()
+        {
+           return context.Categories.ToList();
+        }
+
+        
+
+        public Categories Add (Categories category)
+        {
+
+            context.Categories.Add(category);
+            context.SaveChanges();
+
+            return category;
+        }
+
+        public Categories Delete (int idCategory)
+        {
+            Categories category = null;
+
+            try
+            {
+
+                category = context.Categories.Find(idCategory);
+                context.Categories.Remove(category);
+                context.SaveChanges();
+            }
+
+
+
+            catch (System.Data.Entity.Infrastructure.DbUpdateException)
+            {
+                throw new UpdateException();
+
+            }
+           
+
+            return category;
+
+        }
+
+        public Categories Update (int id, string name, string description, byte[] picture)
+        {
+
+            Categories categoryUpdate = context.Categories.Find(id);
+
+            categoryUpdate.CategoryName = name;
+            categoryUpdate.Description = description;
+            categoryUpdate.Picture = picture;
+            context.SaveChanges();
+
+            return categoryUpdate;
+        }
+        
+       
+    }
+}
